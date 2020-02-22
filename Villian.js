3f624fd3b314c655d6a3c2c234b3239cc1b88ad9
ScottGrant528 <56115663+ScottGrant528@users.noreply.github.com>
2020-02-22 14:19:59 +0000

    Add files via upload

---
+++ Villian.js
@@ -0,0 +1,24 @@
+/*class myClass { 
+    //Class methods 
+        constructor(){}
+        method1(){}
+        method2(){}
+        method3(){}
+}
+*/ 
+/*
+List of bad guys 
+Evil King (best unit)
+Imperial Guard (higher difficulty)
+Vetran Soldier (high difficulty)
+Soldier (med difficulty)
+Regular Guard (low difficulty)
+Light Guard (med difficulty)
+*/ 
+
+//Class to contain the bad guys 
+class badGuy {
+    constructor(){
+        
+    }
+}
