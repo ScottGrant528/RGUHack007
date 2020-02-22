37bfb637155995b6cb67f7c0970ca0f739ba3b1d
KavehNejad <56351092+KavehNejad@users.noreply.github.com>
2020-02-22 19:47:42 +0000

    Update Locations.js

--- Locations.js
+++ Locations.js
@@ -1,4 +1,4 @@
-class Locations{
+export class Location{
     constructor(name,items,discription){
         this.name = name;
         this.items = items;
@@ -6,6 +6,10 @@ class Locations{
 }
 
 
-var cell = new Location("Cell", ["Spear","apple"],"This dark old cell is full of broken stuff and the walls are damaged");
-var hallwayOne = new Location("Hallway",["WoodenBox"],"The long hallway with a door at the end and a door halfway down");
-var guardsRoom = new Location("Guards Room",["Spear"],"The guards room has several boxs at the foots of beds and 2 doors on either side");
+var cell = new Locations("Cell", ["Spear","apple"],"This dark old cell is full of broken stuff and the walls are damaged");
+var hallwayOne = new Locations("Hallway",["WoodenBox"],"The long hallway with a door at the end and a door halfway down");
+var guardsRoom = new Locations("Guards Room",["Spear"],"The guards room has several boxs at the foots of beds and 2 doors on either side");
+var wall = new Locations("Wall",[],"");
+
+console.log(cell)
+
