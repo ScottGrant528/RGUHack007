2368fda0d66655a983c8b89669bfab0c595d3c7a
KavehNejad <56351092+KavehNejad@users.noreply.github.com>
2020-02-22 18:17:54 +0000

    Add files via upload

---
+++ Allies.js
@@ -0,0 +1,8 @@
+class Allies{
+    constructor(name){
+        this.name = name;
+        this.health = 100;
+        this.attack = 10;
+        this.defence = 10;
+    }
+}
---
+++ Locations.js
@@ -0,0 +1,11 @@
+class Locations{
+    constructor(name,items,discription){
+        this.name = name;
+        this.items = items;
+    }
+}
+
+
+var cell = new Location("Cell", ["Spear","apple"],"This dark old cell is full of broken stuff and the walls are damaged");
+var hallwayOne = new Location("Hallway",["WoodenBox"],"The long hallway with a door at the end and a door halfway down");
+var guardsRoom = new Location("Guards Room",["Spear"],"The guards room has several boxs at the foots of beds and 2 doors on either side");
