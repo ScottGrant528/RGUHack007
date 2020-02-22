540c30b3b771077389f90d9f6902a1624907732e
rfindlay2 <56404021+rfindlay2@users.noreply.github.com>
2020-02-22 22:03:24 +0000

    Update Locations.js

--- Locations.js
+++ Locations.js
@@ -6,10 +6,14 @@ export class Location{
 }
 
 
-var cell = new Locations("Cell", ["Spear","apple"],"This dark old cell is full of broken stuff and the walls are damaged");
-var hallwayOne = new Locations("Hallway",["WoodenBox"],"The long hallway with a door at the end and a door halfway down");
+var cell = new Locations("Cell", [rock,],"This dark old cell is full of broken stuff and the walls are damaged");
+var hallwayOne = new Locations("Hallway",[chestHallway],"The long hallway with a door at the end and a door halfway down");
 var guardsRoom = new Locations("Guards Room",["Spear"],"The guards room has several boxs at the foots of beds and 2 doors on either side");
 var wall = new Locations("Wall",[],"");
-
+var throneRoom = new Locations("Throne Room",[],"The throne room is a huge room with a single gold throne at the far side, with a king sitting upon it");
+var chapel = new Locations("The Chapel",[smallChest],"A big room with wooden pews and a big stained glass window");
+var kitchen = new Locations("Kitchen",[bread,applepie],"medium sized room with a low ceiling,oak counter tops with vegetables packed ontop");
+var greatHall = new Locations("The Great Hall",[],"A huge space with a U-shaped wooden table by a great fire");
+var armory = new Locations("Armory Room",[],"A large room with many weapon racks, and also weapons mounted on the walls ");
 console.log(cell)
 
