aaa3bc4f1ee6c19ebd3478805a35b931430eddd3
KavehNejad <56351092+KavehNejad@users.noreply.github.com>
2020-02-22 23:27:02 +0000

    Update Battle.js

--- Battle.js
+++ Battle.js
@@ -2,12 +2,10 @@
 // ambusher attacks first 
 // function battle, hero and opponents objects, var for who gets first attack defined outside, 
 
-var attackMiss = true; 
-var playerFirst = true;
-var turn = ""; 
 
 //Agility function to decide if attack misses
-function agility(agility){
+
+function hitSuccess(agility){
     if ((math.random()*100) > agility ){
         return false; 
     }
@@ -16,27 +14,67 @@ function agility(agility){
     }
 }
 
-//Function to decide who attacks first
-function attackFirst(heroAttack, aiAttack){
-    if (playerFirst == true){
-        heroAttack; 
-       return turn = "aiNow"
-    }
-    else { 
-        aiAttack;
-      return  turn = "playerNow"
+function battle(Hero, AI, turn){
+    var flee = false;
+    while( Hero.health > 0 && AI.health > 0 && !flee){
+        if (turn == "Hero"){
+            while(!Hero.played){
+                var action;
+                do{
+                    alert("in")//add get action
+                }while(action != attack)//add list of actions
+                
+                action = heal;//delete later
+
+                switch (action){
+                    case "heal":
+                        Hero.heal();
+                        break;
+                    case "view inventory":
+                        Hero.viewInventory();
+                        break;
+                    case "view stats":
+                        Hero.viewStats();
+                        break;
+                    case "flee":
+                        var fleeSuccess = Math.ceil(Math.random()*100);
+                        if(fleeSuccess > 50){
+                            flee = true;
+                        }
+                        else{
+                            alert("you failed")
+                        }
+                        Hero.played = true;
+                        break;
+                    case "attack":
+                        if(hitSuccess){
+                            var damage = Hero.attack - AI.defence;
+                            if (damage < 0)damage = 0;
+                            AI.health -= damage;
+                            alert("You did " + damage + " damage");
+                        }
+                        else{
+                            alert("your attack was blocked");
+                        }
+                        Hero.played = true;
+                        break;
+                }
+            }
+            Hero.played = false;
+            turn = "AI";
+        }
+        else if(AI.health > 0 && turn == "AI"){
+            
+            if(hitSuccess){
+                var damage = AI.attack - Hero.defence;
+                if (damage < 0)damage = 0;
+                AI.health -= damage;
+                alert("the " + AI.name + " did " + damage + " damage");
+            }
+            else{
+                alert("you blocked the attack");
+            }
+            turn = "Hero"
+        }
     }
 }
-
-//Function to impliment the turn based system
-function turnBased(heroAttack, aiAttack){
-    if (turn == "playerNow"){
-        heroAttack;
-        return turn = "aiNow"
-    }
-    else {
-        aiAttack;
-        return turn = "playerNow"
-    }
-}
-
