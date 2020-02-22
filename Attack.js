2dd6e07a13a4c29f9c4ff37cf007d66864d4ed29
ScottGrant528 <56115663+ScottGrant528@users.noreply.github.com>
2020-02-22 20:49:53 +0000

    Add files via upload

--- Attack.js
+++ Attack.js
@@ -1,15 +1,6 @@
 var playerFirst = true;
 var attackMiss = true; 
 
-function agility(agility){
-    if ((math.random()*100) > agility ){
-        return false; 
-    }
-    else {
-        return true; 
-    }
-}
-
 function heroAttack(hero, ai){ 
     ai.health =- (hero.attack - ai.defence);
 };
--- Battle.js
+++ Battle.js
@@ -4,12 +4,39 @@
 
 var attackMiss = true; 
 var playerFirst = true;
+var turn = ""; 
 
+//Agility function to decide if attack misses
+function agility(agility){
+    if ((math.random()*100) > agility ){
+        return false; 
+    }
+    else {
+        return true; 
+    }
+}
+
+//Function to decide who attacks first
 function attackFirst(heroAttack, aiAttack){
     if (playerFirst == true){
         heroAttack; 
+       return turn = "aiNow"
     }
     else { 
         aiAttack;
+      return  turn = "playerNow"
     }
 }
+
+//Function to impliment the turn based system
+function turnBased(heroAttack, aiAttack){
+    if (turn == "playerNow"){
+        heroAttack;
+        return turn = "aiNow"
+    }
+    else {
+        aiAttack;
+        return turn = "playerNow"
+    }
+}
+
