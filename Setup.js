3a2441f1586284a4daa23c4d1c43811482dc75c9
rfindlay2 <56404021+rfindlay2@users.noreply.github.com>
2020-02-23 09:04:55 +0000

    Update Setup.js

--- Setup.js
+++ Setup.js
@@ -1,5 +1,10 @@
 
-var map = [[wall,wall,wall,wall,wall,wall,wall,wall,wall,wall,wall],
-           [wall,cell,wall,wall,wall,wall,wall,wall]
+var map = [[wall,wall,wall,wall,wall,wall,wall],
+           [wall,cell,wall,wall,wall,wall,wall],
+           [wall,hallwayOne,guardsRoom,wall,wall],
+           [wall,hallway,wall,wall,wall,wall,wall],
+           [wall,chapel,kitchen,wall,wall,wall,wall],
+           [wall,armory,greatHall,wall,wall,wall,wall],
+           [wall,wall,throneRoom ,wall,wall,wall,wall],
     ]
 alert(map[1][2].name)
